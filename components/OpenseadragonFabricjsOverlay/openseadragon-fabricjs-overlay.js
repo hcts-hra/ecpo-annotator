007a823d24d7edc221571957f8175c36d96e319d
Juri Leino <github@line-o.de>
2019-02-07 01:33:22 +0100

    fix (points): align poly points and handles
    Fixes #35
    
    Both with
    - radius 5
    - stroke #336
    - strokeWidth 1
    - full opacity

--- components/OpenseadragonFabricjsOverlay/openseadragon-fabricjs-overlay.js
+++ components/OpenseadragonFabricjsOverlay/openseadragon-fabricjs-overlay.js
@@ -5,7 +5,7 @@
     console.error("[openseadragon-canvas-overlay] requires OpenSeadragon");
     return;
   }
-
+  
   const colors = [
     "#e67c73","#db4437","#c53929","#7baaf7","#4285f4","#3367d6","#57bb8a","#0f9d58","#0b8043","#f7cb4d","#f4b400","#f09300","#f44336","#e53935","#d32f2f","#c62828","#b71c1c","#ff8a80","#ff5252","#ff1744","#d50000","#e91e63","#d81b60","#c2185b","#ad1457","#880e4f","#ff80ab","#ff4081","#f50057","#c51162","#ce93d8","#ba68c8","#ab47bc","#9c27b0","#8e24aa","#7b1fa2","#6a1b9a","#4a148c","#ea80fc","#e040fb","#d500f9","#aa00ff","#b39ddb","#9575cd","#7e57c2","#673ab7","#5e35b1","#512da8","#4527a0","#311b92","#b388ff","#7c4dff","#651fff","#6200ea","#9fa8da","#7986cb","#5c6bc0","#3f51b5","#3949ab","#303f9f","#283593","#1a237e","#8c9eff","#536dfe","#3d5afe","#304ffe","#90caf9","#64b5f6","#42a5f5","#2196f3","#1e88e5","#1976d2","#1565c0","#0d47a1","#82b1ff","#448aff","#2979ff","#2962ff","#b3e5fc","#81d4fa","#4fc3f7","#29b6f6","#03a9f4","#039be5","#0288d1","#0277bd","#01579b","#80d8ff","#40c4ff","#00b0ff","#0091ea","#b2ebf2","#80deea","#4dd0e1","#26c6da","#00bcd4","#00acc1","#0097a7","#00838f","#006064","#84ffff","#18ffff","#00e5ff","#00b8d4","#b2dfdb","#80cbc4","#4db6ac","#26a69a","#009688","#00897b","#00796b","#00695c","#004d40","#a7ffeb","#64ffda","#1de9b6","#00bfa5","#c8e6c9","#a5d6a7","#81c784","#66bb6a","#4caf50","#43a047","#388e3c","#2e7d32","#1b5e20","#b9f6ca","#69f0ae","#00e676","#00c853","#dcedc8","#c5e1a5","#aed581","#9ccc65","#8bc34a","#7cb342","#689f38","#558b2f","#33691e","#ccff90","#b2ff59","#76ff03","#64dd17","#f0f4c3","#e6ee9c","#dce775","#d4e157","#cddc39","#c0ca33","#afb42b","#9e9d24","#827717","#f4ff81","#eeff41","#c6ff00","#aeea00","#fff176","#ffee58","#ffeb3b","#fdd835","#fbc02d","#f9a825","#f57f17","#ffff00","#ffea00","#ffd600","#ffe082","#ffd54f","#ffca28","#ffc107","#ffb300","#ffa000","#ff8f00","#ff6f00","#ffe57f","#ffd740","#ffc400","#ffab00","#ffcc80","#ffb74d","#ffa726","#ff9800","#fb8c00","#f57c00","#ef6c00","#e65100","#ffd180","#ffab40","#ff9100","#ff6500","#ffab91","#ff8a65","#ff7043","#ff5722","#f4511e","#e64a19","#d84315","#bf360c","#ff9e80","#ff6e40","#ff3d00","#dd2c00","#d7ccc8","#bcaaa4","#a1887f","#8d6e63","#795548","#6d4c41","#5d4037","#4e342e","#3e2723","#bdbdbd","#9e9e9e","#757575","#616161","#424242","#212121","#cfd8dc","#b0bec5","#90a4ae","#78909c","#607d8b","#546e7a","#455a64","#37474f","#263238"    
   ]
@@ -90,9 +90,9 @@
     },
     pointStyle: {
       radius: 5,
-      fill: '#ffffff',
-      stroke: '#333333',
-      strokeWidth: 0.5,
+      fill: 'transparent',
+      stroke: '#336',
+      strokeWidth: 1,
       selectable: false,
       hasBorders: false,
       hasControls: false,
@@ -114,10 +114,10 @@
       objectCaching:false
     },
     polyPointStyle: {
+      radius: 5,
       stroke:'#336',
-      strokeWidth: 2,
-      fill: '#aaf',
-      opacity: 0.3,
+      strokeWidth: 1,
+      fill: 'transparent',
       selectable: false,
       hasBorders: false,
       hasControls: false,
@@ -354,7 +354,7 @@
       let color = this.unassignedColor
       if ( object.data && object.data.label) {
         color = object.data.label.color
-        }  
+      }
       if (this._annotator.mode === this._annotator.modes.GROUP) {
         color = object.data.color || 'purple'
       }
@@ -442,7 +442,7 @@
           if (this._isPointHandle(options.target)) {
             console.log('point', options.target)
             this.currentPointHandle = options.target
-            this.pointArray.forEach(point => point.set({ fill: 'white' }))
+            this.pointArray.forEach(point => point.set({ fill: 'transparent' }))
             options.target.set({ fill: 'tomato' })
             this._fabricCanvas.renderAll()
             break
